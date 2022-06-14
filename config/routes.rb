b15548ab655d8d65964a1ce7d7f8527fc31d5bea
Rkanraeden <lecortezc@gmail.com>
2022-06-13 22:13:00 -0400

    views clients and stories updated

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :stories
   resources :pets
   resources :clients
+  root "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
